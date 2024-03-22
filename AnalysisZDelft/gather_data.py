e82d61f80bf1d8d908866c21e6a1e19396834e7a
zdelft <zdelft@sfu.ca>
2024-03-22 13:45:49 -0700

    Created text file to hold API key, gather_data.py now opens file and moves key to variable for later use - zdelft

--- AnalysisZDelft/gather_data.py
+++ AnalysisZDelft/gather_data.py
@@ -3,3 +3,21 @@
 #
 #
 
+# Gather Data
+
+# Text file where key is saved
+KEY_FILE = 'transit_api_key.txt'
+
+# Function to get transit API key from text file
+def getAPIKey():
+    file = open(KEY_FILE, 'r')
+    key = file.read().rstrip()
+    file.close()
+    return key
+
+def main():
+    API_KEY = getAPIKey()
+    
+
+if __name__ == '__main__':
+    main()
