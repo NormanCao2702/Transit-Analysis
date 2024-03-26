d83f809db314b132af4d4ea9c011c5b4ed6e1bb4
zdelft <zdelft@sfu.ca>
2024-03-25 18:12:36 -0700

    problem - get response but no data, idea may not be possible

--- AnalysisZDelft/gather_data.py
+++ AnalysisZDelft/gather_data.py
@@ -7,7 +7,7 @@
 
 import requests
 import csv
-from datetime import date
+from datetime import datetime, time, timedelta
 
 # Text file where key is saved
 KEY_FILE = 'transit_api_key.txt'
@@ -16,12 +16,8 @@ API_URL = 'https://external.transitapp.com/v3/otp/plan'
 # file csv is saved to
 OUT_FILE = 'transit_route.csv'
 # Latitudes and longitudes for SFU and a chosen "home" location
-SFU_LATLONG = '49.2781, 122.9199'
-HOME_LATLONG = '49.2328, 123.0661'
-# Variables to hold start times and finish times (UTC)
-HR, MIN = 12, 0
-# Date for request
-DATE = date.today()
+SFU_LATLONG = 'SFU::49.2781,122.9199'
+HOME_LATLONG = 'HOME::49.2328,123.0661'
 
 # Function to get transit API key from text file
 def getAPIKey():
@@ -35,18 +31,33 @@ def main():
     # Get API key and request information
     API_KEY = getAPIKey()
     
+    # Time must be in UTC
+    DATE = datetime.combine(datetime.now().date(), time(12, 0)) #+ timedelta(days=1)
+    END_DATE = DATE + timedelta(days=1)
+    print(f"{DATE}")
+    
     # Get file ready for writing
-    file = open(OUT_FILE, mode='a', newline='', encoding='utf-8')
-    while HR != 8 & MIN != 30:
-        responses = requests.get(API_URL, 
-                                 headers = {'apikey': API_KEY, 
-                                            'Accept-Language': 'en'},
-                                 params = {'fromPlace': HOME_LATLONG, 
-                                           'toPlace': SFU_LATLONG,
-                                           'date': f'{DATE.year}-{DATE.month}-{DATE.day + 1}',
-                                           'time': f'{HR}:{MIN}'})
-        if responses.status_code == 200:
-            data = responses.json()
+    file = open(OUT_FILE, mode='w', newline='', encoding='utf-8')
+    writer = csv.writer(file)
+    # Write header for CSV file
+    headers = {'apikey': API_KEY, 'Accept-Language': 'en'}
+    params = {'fromPlace': HOME_LATLONG,
+              'toPlace': SFU_LATLONG,
+              'date': f'{DATE.date()}',
+              'time': f'{DATE.hour}:{DATE.minute}'}
+    
+    responses = requests.get(API_URL, headers=headers, params = params)
+    
+    if responses.status_code == 200:
+        data = responses.json()
+        plan = data['plan']
+        print(len(plan['itineraries']))
+        for itinerarie in plan['itineraries']:
+            print(itinerarie)
+    else:
+        print('fail')
+    
+   
     
 
 if __name__ == '__main__':
